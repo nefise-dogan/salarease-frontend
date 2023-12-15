cc4e22289081127a151705b03627aefab78c4d18
Nefise Doğan <enefisedogan@gmail.com>
2023-12-15 17:32:20 +0300

    Add analyzer demo video

--- src/app/analyzer/page.tsx
+++ src/app/analyzer/page.tsx
@@ -1,8 +1,13 @@
 function Analyzer() {
     return (
-        <div>
-            <h1>Analyzer</h1>
-            <p>This is the analyzer page.</p>
+        <div className="flex flex-col items-center">
+            <h1 className="self-center text-center text-4xl font-extralight leading-snug text-black">
+                Analyze the salary trends
+            </h1>
+            <p className="mt-8">Here is our analyzer demo with PowerBI. Contact us for more information.</p>
+            <video className="mt-8" controls width="80%">
+                <source src="/demo.mp4" type="video/mp4" />
+            </video>
         </div>
     );
 }
